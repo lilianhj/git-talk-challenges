38560dd79b9a597f5f9a3314ce9d2c404d034972
Mark Cohen <m@mpc.sh>
2019-01-22 11:29:55 -0600

    Make all the tests pass

--- test_main.py
+++ test_main.py
@@ -7,10 +7,10 @@ def test_that_strings_work():
     assert "Hello, world!" == "Hello, " + "world!"
 
 def test_echo():
-    assert echo("Hi, world!") == "Hi, world!"
+    assert echo("Hi, world!") == "	Hi, world!"
 
 def test_multi_echo():
-    assert echo("Hi, ") + echo("world!") == "Hi, world!"
+    assert echo("Hi, ") + echo("world!") == "	Hi,	world!"
 
 def test_linear_echo():
-    assert echo("Hi, " + "world!") == echo("Hi, ") + echo("world!")
+    assert echo("Hi, " + "	world!") == echo("Hi, ") + " " + echo("world!")
