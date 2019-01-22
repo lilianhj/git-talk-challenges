4a2ea7c016025666a1fe2f22e5907f68242cbc0a
Mark Cohen <m@mpc.sh>
2019-01-22 11:15:25 -0600

    Add a test for the echo library

--- test_main.py
+++ test_main.py
@@ -1,5 +1,10 @@
+from main import echo
+
 def test_sanity():
     assert True
 
 def test_that_strings_work():
     assert "Hello, world!" == "Hello, " + "world!"
+
+def test_echo():
+    assert echo("Hi, world!") == "Hi, world!"
